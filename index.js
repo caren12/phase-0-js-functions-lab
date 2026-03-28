c4aa3bff4f1ba4eb58b95d1b70dd2ef95905ea26
Caren Chemjor <carenjeropchemjor@gmail.com>
2026-03-28 22:54:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 
 
 
+//This function calculate tax based on income
+function calcculateTax(amount) {
+    return amount * 0.10}
+
+//Converts to uppercase
+function convertToUppercare(text){
+    return text.toUpperCase()
+}
+
+
+//Finds the maximum
+function findMaximum(num1, num3) 
+    if (num1 > num2) {
+        return num1
+    }
+    else {return num2
+
+    }
+
+
+//is palindrome
+function is palindrome(word)  {
+    let reversed = word.split("").reverse().join("");
+    return word === reversed
+}  
+
+//
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discountAmount = (originalPrice * discountPercentage/100) ;
+    return originalPrice - discountAmount
+}
+
+
+
+
+
+
+
+
+
+
 
 
 // This is required for the test to function properly  
