b8f23fa7706942bb1f60f08f569832a964e7653d
Caren Chemjor <carenjeropchemjor@gmail.com>
2026-03-29 00:57:53 +0300

    final submission

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-
-
-
-//This function calculate tax based on income
-function calcculateTax(amount) {
-    return amount * 0.10}
-
-//Converts to uppercase
-function convertToUppercare(text){
-    return text.toUpperCase()
+function calculateTax(amount) {
+    return amount * 0.10;
 }
 
+// Converts to uppercase
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
-//Finds the maximum
-function findMaximum(num1, num3) 
+// Finds the maximum
+function findMaximum(num1, num2) {
     if (num1 > num2) {
-        return num1
+        return num1;
+    } else {
+        return num2;
     }
-    else {return num2
+}
 
-    }
-
-
-//is palindrome
-function is palindrome(word)  {
+// Is palindrome
+function isPalindrome(word) {
     let reversed = word.split("").reverse().join("");
-    return word === reversed
-}  
+    return word === reversed;
+}
 
-//Discounted price
+// Discounted price
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
-    let discountAmount = (originalPrice * discountPercentage/100) ;
-    return originalPrice - discountAmount
+    let discountAmount = (originalPrice * discountPercentage / 100);
+    return originalPrice - discountAmount;
 }
 
 
-
-
-
-
-
-
-
-
-
+module.exports = {
+    calculateTax,
+    convertToUpperCase,
+    findMaximum,
+    isPalindrome,
+    calculateDiscountedPrice
+};
